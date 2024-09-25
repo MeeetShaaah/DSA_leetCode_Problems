b652d01c8e0b8f1f4532264c6dd90d8a2778b30a
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-09-25 18:47:08 -0400

    Time: 2 ms (60.06%), Space: 41.6 MB (26.4%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,14 +5,14 @@ class Solution {
         int s_pointer = 0;
         int t_pointer = 0;
 
-        while (t_pointer < t.length()){
+        while (s_pointer < s.length() && t_pointer < t.length()){
             if (t.charAt(t_pointer) == s.charAt(s_pointer)){
                 s_pointer++;
 
-                if(s_pointer == s.length()) return true;
+                // if(s_pointer == s.length()) return true;
             }
             t_pointer++;
         }
-        return false;
+        return s_pointer == s.length();
     }
 }
