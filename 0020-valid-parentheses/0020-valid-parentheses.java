2957ef8c1ff5cb247d0ff21dd070bd5a74ce4dfc
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-11-17 20:40:12 -0500

    Time: 2 ms (97.2%), Space: 41.7 MB (41.64%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,17 +6,17 @@ class Solution {
             if(c == '(' || c == '{' || c == '['){
                 stack.push(c);
             }else{
-                if(stack.isEmpty()) return false;
+                if (stack.isEmpty()) return false;
 
                 char top = stack.peek();
 
                 if((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')){
                     stack.pop();
-                }else{
+                }else {
                     return false;
                 }
             }
         }
-        return stack.isEmpty();
+        return stack.isEmpty(); 
     }
 }
