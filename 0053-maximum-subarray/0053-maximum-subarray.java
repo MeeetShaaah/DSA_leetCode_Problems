e83eb8172c5778b9809b5f60b2001a671f7aefad
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-03 12:40:10 -0500

    Time: 1 ms (99.39%), Space: 56.6 MB (88.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,20 +1,34 @@
-// Approch - 2, known as Kadane's Algorithm, very it is just a math
-
+// Same as 2nd approch, but the implementation style is differnet..
 class Solution {
     public int maxSubArray(int[] nums) {
+      int sum = 0;
+      int max = Integer.MIN_VALUE;
 
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-
-        for (int num : nums){
-            sum += num;
-
-            if (sum > max) max = sum;
-
-            if (sum < 0) sum = 0;
-        }
-        return max;
+      for (int num : nums){
+        sum = Math.max(sum + num, num);
+        max = Math.max(max, sum);
+      }  
+      return max;
     }
 }
+
+
+// Approch - 2, known as Kadane's Algorithm, very it is just a math
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+
+//         int sum = 0;
+//         int max = Integer.MIN_VALUE;
+
+//         for (int num : nums){
+//             sum += num;
+
+//             if (sum > max) max = sum;
+
+//             if (sum < 0) sum = 0;
+//         }
+//         return max;
+//     }
+// }
 
 // Approch - 1, brute force, 2 for loops, 1st to pick the element and 2nd to calculate the sum and then Math.max
