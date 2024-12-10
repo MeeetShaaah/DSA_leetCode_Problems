23ed058b4b86b1b39cdfdeab0799e0b2408ff30b
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-10 11:48:38 -0500

    Time: 7 ms (61.46%), Space: 42.9 MB (69.4%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,28 +1,46 @@
-//This is my approch, TC and SC - O(n);
+// Approch - 2, 
+
 class Solution {
     public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
 
-        String reversed = new StringBuilder(s).reverse().toString();
-
-        char[] ch = reversed.trim().toCharArray();
         StringBuilder sb = new StringBuilder();
 
-        Stack<Character> chStack = new Stack<>();
-
-        for (int i = 0; i < ch.length; i++){
-            if (ch[i] != ' '){
-                chStack.push(ch[i]);
-            }else if(!chStack.isEmpty()){
-                while (!chStack.isEmpty()){
-                    sb.append(chStack.pop());
-                }
+        for (int i = words.length - 1; i >=0; i--){
+            sb.append(words[i]);
+            if (i != 0){
                 sb.append(" ");
             }
-        }
-
-        while(!chStack.isEmpty()){
-            sb.append(chStack.pop());
         }
         return sb.toString();
     }
 }
+
+//Approc - 1, This is my approch, TC and SC - O(n); but not the optimal one;
+// class Solution {
+//     public String reverseWords(String s) {
+
+//         String reversed = new StringBuilder(s).reverse().toString();
+
+//         char[] ch = reversed.trim().toCharArray();
+//         StringBuilder sb = new StringBuilder();
+
+//         Stack<Character> chStack = new Stack<>();
+
+//         for (int i = 0; i < ch.length; i++){
+//             if (ch[i] != ' '){
+//                 chStack.push(ch[i]);
+//             }else if(!chStack.isEmpty()){
+//                 while (!chStack.isEmpty()){
+//                     sb.append(chStack.pop());
+//                 }
+//                 sb.append(" ");
+//             }
+//         }
+
+//         while(!chStack.isEmpty()){
+//             sb.append(chStack.pop());
+//         }
+//         return sb.toString();
+//     }
+// }
