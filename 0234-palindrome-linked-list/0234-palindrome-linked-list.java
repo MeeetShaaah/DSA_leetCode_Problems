e62f26086b202a90b741f7b20f6f19b2d3251a19
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-16 15:50:00 -0500

    Time: 9 ms (26.58%), Space: 72.2 MB (5.03%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Approch - 2, hare and tortoise method,....!!!
+class Solution {
+    public ListNode reverse(ListNode head){
+        if (head == null || head.next == null) return head;
+
+        ListNode newHead = reverse(head.next);
+
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+
+        if( head == null || head.next == null) return true;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode newHead = reverse(slow.next);
+
+        ListNode first = head;
+        ListNode second = newHead;
+
+        while(second != null){
+            if (first.val != second.val){
+                return false;
+            }
+
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
+
+// Approch - 1, traverse the LL and add every node to the stack 
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         ListNode current = head;
+//         Stack<Integer> stack = new Stack<>();
+
+//         while (current != null){
+//             stack.push(current.val);
+//             current = current.next;
+//         }
+
+//         current = head;
+
+//         while (current != null){
+//             if (current.val != stack.peek()){
+//                 return false;
+//             }
+
+//             stack.pop();
+//             current = current.next;
+//         }
+//         return true;
+//     }
+// }
