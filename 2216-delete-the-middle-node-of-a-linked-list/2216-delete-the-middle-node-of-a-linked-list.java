727f3396f49a0d40e611f456e11d600a4f3bd8df
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-16 22:30:08 -0500

    Time: 3 ms (99.85%), Space: 65.2 MB (18.55%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -8,15 +8,13 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- 
+
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         if(head == null || head.next == null) return null;
 
         ListNode slow = head;
-        ListNode fast = head;
-
-        fast = fast.next.next; // The reason is that if both fast and slow pointers move at the same pace, // the slow pointer will stop at the middle node, and removing 'slow.next' would skip the middle node itself. To remove the middle node, we need the slow pointer to stop one node before the middle. To achieve this, we initialize fast = fast.next.next.
+        ListNode fast = head.next.next; // The reason is that if both fast and slow pointers move at the same pace, // the slow pointer will stop at the middle node, and removing 'slow.next' would skip the middle node itself. To remove the middle node, we need the slow pointer to stop one node before the middle. To achieve this, we initialize fast = fast.next.next.
 
         while(fast != null && fast.next != null){
             slow = slow.next;
