06142f2db5751bd1aadb2b96ee47a6497e469c70
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-20 14:20:43 -0500

    Time: 4 ms (96.83%), Space: 44.8 MB (54.92%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,48 +1,82 @@
 // Approch - 2, i dont understand the 2*value - min
+// class MinStack {
+//     Stack<Long> stack = new Stack<>();
+//     Long min;
+//     public MinStack() {
+//         min = Long.MAX_VALUE;
+//     }
+    
+//     public void push(int val) {
+//         Long value = Long.valueOf(val);
+
+//         if(stack.isEmpty()){
+//             min = value;
+//             stack.push(value);
+//         }else{
+//             if(value < min){
+//                 stack.push(2*value - min);
+//                 min = value;
+//             }else{
+//                 stack.push(value);
+//             }
+//         }
+//     }
+    
+//     public void pop() {
+//         if(stack.isEmpty()) return;
+
+//         Long value = stack.pop();
+
+//         if(value < min){
+//             min = 2 * min -value;
+//         }
+//     }
+    
+//     public int top() {
+//         Long value = stack.peek();
+
+//         if(value < min){
+//             return min.intValue();
+//         }
+//         return value.intValue();
+//     }
+    
+//     public int getMin() {
+//         return min.intValue();
+//     }
+// }
+
+// Using extra space,
+
 class MinStack {
-    Stack<Long> stack = new Stack<>();
-    Long min;
-    public MinStack() {
-        min = Long.MAX_VALUE;
+Stack<Integer> stack = new Stack<>();
+Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {        
     }
     
     public void push(int val) {
-        Long value = Long.valueOf(val);
+        stack.push(val);
 
-        if(stack.isEmpty()){
-            min = value;
-            stack.push(value);
-        }else{
-            if(value < min){
-                stack.push(2*value - min);
-                min = value;
-            }else{
-                stack.push(value);
-            }
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
         }
     }
     
     public void pop() {
-        if(stack.isEmpty()) return;
-
-        Long value = stack.pop();
-
-        if(value < min){
-            min = 2 * min -value;
+        if(stack.peek().equals(minStack.peek())){
+            minStack.pop();
         }
+
+        stack.pop();
     }
     
     public int top() {
-        Long value = stack.peek();
-
-        if(value < min){
-            return min.intValue();
-        }
-        return value.intValue();
+        return stack.peek();
     }
     
     public int getMin() {
-        return min.intValue();
+        return minStack.peek();
     }
 }
 
