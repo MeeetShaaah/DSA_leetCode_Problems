3fb60aec69eac6fa9ac4b0a016c4402f2375e0bc
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-25 16:46:12 -0500

    Time: 0 ms (100%), Space: 46.6 MB (29.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,48 @@
-// Approch - 2, using stack;
+// Approch - 3, using two pointers.
 class Solution {
     public int trap(int[] height) {
-        Stack<Integer> stack = new Stack<>();
+        int left = 0;
+        int right = height.length - 1;
         int trappedWater = 0;
-        int current = 0;
+        int left_max = 0, right_max = 0;
 
-        while(current < height.length){
-            while (!stack.isEmpty() && height[current] > height[stack.peek()]){
-                int top = stack.peek();
-                stack.pop();
-                if(stack.isEmpty()) break;
-                int distance = current - stack.peek() - 1;
-                int boundedHeight = Math.min(height[current], height[stack.peek()]) - height[top];
-                trappedWater += distance * boundedHeight;
+        while (left < right){
+            if (height[left] < height[right]){
+                left_max = Math.max(left_max, height[left]);
+                trappedWater += left_max - height[left];
+                left++;
+            }else{
+                right_max = Math.max(right_max, height[right]);
+                trappedWater += right_max - height[right];
+                right--;
             }
-            stack.push(current);
-            current++;
         }
-        return trappedWater;
+        return trappedWater;  
     }
 }
+
+// Approch - 2, using stack;
+// class Solution {
+//     public int trap(int[] height) {
+//         Stack<Integer> stack = new Stack<>();
+//         int trappedWater = 0;
+//         int current = 0;
+
+//         while(current < height.length){
+//             while (!stack.isEmpty() && height[current] > height[stack.peek()]){
+//                 int top = stack.peek();
+//                 stack.pop();
+//                 if(stack.isEmpty()) break;
+//                 int distance = current - stack.peek() - 1;
+//                 int boundedHeight = Math.min(height[current], height[stack.peek()]) - height[top];
+//                 trappedWater += distance * boundedHeight;
+//             }
+//             stack.push(current);
+//             current++;
+//         }
+//         return trappedWater;
+//     }
+// }
 
 // Approch - 1, for every index, find max and min.
 // class Solution {
