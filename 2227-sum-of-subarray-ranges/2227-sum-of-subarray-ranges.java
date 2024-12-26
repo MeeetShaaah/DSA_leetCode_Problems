6608bf4790a239368d6f90c2a3b9ffb816bb4d73
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2024-12-26 13:38:02 -0500

    Time: 17 ms (64.5%), Space: 44.4 MB (60.79%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -0,0 +1,32 @@
+// Approch - 2, monotonic stack,
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        long ans = 0;
+
+        for(int i = 0; i <= nums.length; i++){
+            // we are finding the subarrays with the current element minimum;   
+            while (!stack.isEmpty() && (i == nums.length || nums[i] <= nums[stack.peek()])){
+                int mid = stack.pop();
+                int leftBound = stack.isEmpty() ?  -1 : stack.peek();
+                int rightBound = i;
+                ans -= (long) nums[mid] * (mid - leftBound) * (rightBound - mid);
+            }
+            if (i < nums.length){stack.push(i);}
+        }
+        stack.clear();
+        for(int i = 0; i <= nums.length; i++){
+            // we are finding the subarrays with the current element maximum;   
+            while (!stack.isEmpty() && (i == nums.length || nums[i] >= nums[stack.peek()])){
+                int mid = stack.pop();
+                int leftBound = stack.isEmpty() ?  -1 : stack.peek();
+                int rightBound = i;
+                ans += (long) nums[mid] * (mid - leftBound) * (rightBound - mid);
+            }
+            if (i < nums.length){stack.push(i);}
+        }
+        return ans;
+    }
+}
+
+// Approch - 1(self), brute force, one pointer from left and other from right, TC O(n2);
