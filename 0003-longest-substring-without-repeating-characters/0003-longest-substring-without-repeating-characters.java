6804888e02500e280e126cf6c210e52fe46256f3
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-01 16:47:22 -0500

    Time: 5 ms (87.05%), Space: 44.9 MB (36.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,37 @@
-// Approch - 2, find optimal approach, have to decrease TC from O(n2) to O(n), the question is that how to do this?
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int max = 0;
-        Set<Character> set = new HashSet<>();
-
+        int left = 0, max = 0;
+        Map<Character, Integer> map = new HashMap<>();
         for(int right = 0; right < s.length(); right++){
-            while(set.contains(s.charAt(right))){
-                set.remove(s.charAt(left));
-                left++;
+            if(map.containsKey(s.charAt(right))){
+                left = Math.max(map.get(s.charAt(right)) + 1, left); // Very useful confdition... 
             }
-
-            set.add(s.charAt(right));
             max = Math.max(max, right - left + 1);
+            map.put(s.charAt(right), right);
         }
         return max;
     }
 }
+
+// Approch - 2, find optimal approach, have to decrease TC from O(n2) to O(n), the question is that how to do this?
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int left = 0;
+//         int max = 0;
+//         Set<Character> set = new HashSet<>();
+
+//         for(int right = 0; right < s.length(); right++){
+//             while(set.contains(s.charAt(right))){
+//                 set.remove(s.charAt(left));
+//                 left++;
+//             }
+
+//             set.add(s.charAt(right));
+//             max = Math.max(max, right - left + 1);
+//         }
+//         return max;
+//     }
+// }
 
 // Approch - 1, the brute force one will be like, start from index - 0 and inner loop from index i, check every possible substring and count, TC - O(n2)
 // class Solution {
