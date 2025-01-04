1b5580266c0e4918f52e2c1a24901b6d0a49ae0e
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-03 19:05:29 -0500

    Time: 60 ms (10.79%), Space: 115.5 MB (37.76%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,9 +1,6 @@
-//  If you're using the key to access the cache (via get(key) or put(key, value)), and if you store the key directly in the DLL node, you can quickly find, update, or remove the node from both the DLL and the HashMap in constant time (O(1)).
-//  If you were passing values instead of keys (i.e., you'd only access the cache by value), then theoretically, you wouldn't need the key in the DLL because there wouldn't be a need to track which node corresponds to which key. However, this would break the functionality of a key-based LRU Cache, which is why you store both the key and the value in the DLL node in this design.
-//  Thus, storing the key in the DLL is necessary for efficient key-based access and removal in an LRU Cache.
 class Node{
-    Node prev, next;
     int key, value;
+    Node prev, next;
 
     Node(int key, int value){
         this.key = key;
@@ -11,16 +8,16 @@ class Node{
     }
 }
 
-class LRUCache {    
+class LRUCache {
+    Map<Integer, Node> map = new HashMap<>();
     Node head = new Node(0,0);
     Node tail = new Node(0,0);
-    Map<Integer, Node> map = new HashMap<>();
-    int capacity;
+    int capacity = 0; 
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
         head.next = tail;
-        tail.prev = head;   
+        tail.prev = head;
     }
     
     public int get(int key) {
@@ -45,9 +42,9 @@ class LRUCache {
     }
 
     private void removeNode(Node node){
-        map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
+        map.remove(node.key);
     }
 
     private void addNode(Node node){
