3685373e7ebaf8adb109cc9b00c2e6d5497e81e2
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-05 16:29:10 -0500

    Time: 13 ms (55.23%), Space: 44.8 MB (37.46%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,21 +1,43 @@
 class Solution {
     public int numberOfSubstrings(String s) {
+        int[] count = {0,0,0};
+        int res = 0;
         int left = 0;
-        int right = 0;
-        int[] freq = new int[3];
-        int answer = 0;
 
-        while(right < s.length()){
-            freq[s.charAt(right) - 'a']++;
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+            count[ch-'a']++;
 
-            while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0){
-                answer += s.length() - right;
-
-                freq[s.charAt(left) - 'a']--;
+            while(count[0] >= 1 && count[1] >= 1 && count[2] >= 1){
+                count[s.charAt(left) - 'a']--;
                 left++;
             }
-        right++;
+
+            res += left;
         }
-        return answer;
+
+        return res;
     }
 }
+
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int left = 0;
+//         int right = 0;
+//         int[] freq = new int[3];
+//         int answer = 0;
+
+//         while(right < s.length()){
+//             freq[s.charAt(right) - 'a']++;
+
+//             while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0){
+//                 answer += s.length() - right;
+
+//                 freq[s.charAt(left) - 'a']--;
+//                 left++;
+//             }
+//         right++;
+//         }
+//         return answer;
+//     }
+// }
