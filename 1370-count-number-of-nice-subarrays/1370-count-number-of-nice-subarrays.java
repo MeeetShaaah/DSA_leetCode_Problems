a07d5f28133554cbfbceee2ed156c5c7967a3f71
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-05 14:42:54 -0500

    Time: 41 ms (18.3%), Space: 55.9 MB (5.94%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,28 +1,50 @@
+// I have to learn how to use prefix sum and hashmap, I am not getting how this approch works.
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return atMost(nums, k) - atMost(nums, k-1);
-    }
+        int currentSum = 0;
+        int subArrays = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(currentSum, 1);
 
-    private int atMost(int[] nums, int k){
-        int left = 0;
-        int right = 0;
-        int count = 0;
-
-        while (right < nums.length){
-            if(nums[right] % 2 != 0){
-                k--;
+        for (int i = 0; i < nums.length; i++){
+            currentSum += nums[i] % 2;
+            if(map.containsKey(currentSum - k)){
+                subArrays += map.get(currentSum - k);
             }
 
-            while (k < 0){
-                if(nums[left] % 2 != 0){
-                    k++;
-                }
-                left++;
-            }
-
-            count += right - left + 1;
-            right++;
+            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
         }
-        return count; 
+        return subArrays;
     }
 }
+
+// Approch - self, for this (return atMost(nums, k) - atMost(nums, k-1);) i saw the solution
+
+// class Solution {
+//     public int numberOfSubarrays(int[] nums, int k) {
+//         return atMost(nums, k) - atMost(nums, k-1);
+//     }
+
+//     private int atMost(int[] nums, int k){
+//         int left = 0;
+//         int right = 0;
+//         int count = 0;
+
+//         while (right < nums.length){
+//             if(nums[right] % 2 != 0){
+//                 k--;
+//             }
+
+//             while (k < 0){
+//                 if(nums[left] % 2 != 0){
+//                     k++;
+//                 }
+//                 left++;
+//             }
+
+//             count += right - left + 1;
+//             right++;
+//         }
+//         return count; 
+//     }
+// }
