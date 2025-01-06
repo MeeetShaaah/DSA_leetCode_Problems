1d899e3d06c37dd1a240ef2d202ed1c13dfedbaf
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-06 13:26:46 -0500

    Time: 49 ms (52.54%), Space: 47.9 MB (32.07%) - LeetHub

---
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -0,0 +1,28 @@
+// Approch - 1, my intuition is, this problem can be solved using sliding window and hashmap, the question arrise how ?
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k){
+        int left = 0;
+        int right = 0;
+        int totalCount = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while(right < nums.length){
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+            while(map.size() > k){
+                map.put(nums[left], map.getOrDefault(nums[left], 0) - 1);
+                if(map.get(nums[left]) == 0){
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            totalCount += right - left + 1;
+            right++;
+        }
+        return totalCount;
+    }
+}
