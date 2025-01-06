bab26f6e9907de10c94bbadfdd4f3c2b82d0ab89
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-06 10:37:50 -0500

    Time: 2 ms (97.15%), Space: 42.5 MB (93.94%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,20 +1,22 @@
-// so basically this question can be solve using sliding window and hashmap, why hashmap? we have to check the frequency and based on that we have to run the condition,  
-// the map size shouold be same as k because, k distinct ccharacters.
-
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         int left = 0;
         int right = 0;
         int maxLen = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        int[] charFreq = new int[256];
+        int counter = 0;
 
         while (right < s.length()){
-            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
-
-            while(map.size()>k){
-                map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) - 1);
-                if(map.get(s.charAt(left)) == 0){
-                    map.remove(s.charAt(left));
+            if(charFreq[s.charAt(right)] == 0){
+                counter++;
+            }
+            
+            charFreq[s.charAt(right)]++;
+            
+            while (counter > k ){ 
+                charFreq[s.charAt(left)]--;
+                if(charFreq[s.charAt(left)] == 0){
+                    counter--;
                 }
                 left++;
             }
@@ -24,3 +26,32 @@ class Solution {
         return maxLen;
     }
 }
+
+// so basically this question can be solve using sliding window and hashmap, why hashmap? we have to check the frequency and based on that we have to run the condition,  
+// the map size shouold be same as k because, k distinct ccharacters.
+
+// class Solution {
+//     public int lengthOfLongestSubstringKDistinct(String s, int k) {
+//         int left = 0;
+//         int right = 0;
+//         int maxLen = 0;
+//         Map<Character, Integer> map = new HashMap<>();
+
+//         while (right < s.length()){
+//             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+
+//             while(map.size()>k){
+//                 map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) - 1);
+//                 if(map.get(s.charAt(left)) == 0){
+//                     map.remove(s.charAt(left));
+//                 }
+//                 left++;
+//             }
+//             maxLen = Math.max(maxLen, right - left + 1);
+//             right++;
+//         }
+//         return maxLen;
+//     }
+// }
+
+
