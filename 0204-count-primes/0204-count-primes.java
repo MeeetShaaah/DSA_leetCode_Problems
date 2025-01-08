f44bbdcd6aa8a35a6b336bf49db0d0c6ff807abd
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-08 15:56:41 -0500

    Time: 100 ms (59.2%), Space: 49.3 MB (53.62%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,4 +1,5 @@
 // didnt do this by self, this is the unique method to find the prime numbers or prime counts,
+// see the video from striver's list step: 8; lec: 3; "Print Prime Factors of a Number" (very useful, learn all methods to find the prime number)
 class Solution {
     public int countPrimes(int n) {
         if(n <= 2) return 0;
