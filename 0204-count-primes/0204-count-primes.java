07117b54237e3803af0edddec01ed1d12c1cf17d
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-08 15:51:58 -0500

    Time: 100 ms (59.2%), Space: 49.5 MB (36.13%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,29 @@
+// didnt do this by self, this is the unique method to find the prime numbers or prime counts,
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for(int  i = 2; i * i < n; i++){
+            if(isPrime[i]){
+                for(int j = i * i; j < n; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
