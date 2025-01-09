4d20a9242724eeba362e47816c655f0766372b47
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-09 12:30:20 -0500

    Time: 0 ms (100%), Space: 46.2 MB (11.17%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -2,18 +2,42 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        return findTarget(nums, target, left, right);
+    }
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
+    private int findTarget(int[] nums, int target, int left, int right){
+        if(left > right) return -1;
 
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] < target){
-                left = mid + 1;
-            }else {
-                right = mid - 1;
-            }
+        int mid = left + (right - left)/2;
+        
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid] < target){
+            return findTarget(nums, target, mid + 1, right);
+        }else {
+            return findTarget(nums, target, left, mid - 1);
         }
-        return -1;
+        // return -1;
     }
 }
+
+// this is the iterative apporach, 
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int left = 0;
+//         int right = nums.length - 1;
+
+//         while(left <= right){
+//             int mid = left + (right - left)/2;
+
+//             if(nums[mid] == target){
+//                 return mid;
+//             }else if(nums[mid] < target){
+//                 left = mid + 1;
+//             }else {
+//                 right = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
