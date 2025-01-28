b635c451bf10080cc477020a435f25816e026e75
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-28 12:00:26 -0500

    Time: 0 ms (100%), Space: 42.3 MB (8.19%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,20 +13,47 @@
  *     }
  * }
  */
+// Approach - 2, Iterative, (There is no wa ythat you can cracck by your own)
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
 
-        helper(root, result);
+        if(root == null) return result;
 
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while(!stack.isEmpty() || current != null){
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+
+            result.add(current.val);
+
+            current = current.right;
+        }
         return result;
     }
-
-    private void helper(TreeNode root, List<Integer> result){
-        if(root == null) return;
-
-        helper(root.left, result);
-        result.add(root.val);
-        helper(root.right, result);
-    }
 }
+
+// Approach - 1, resursive;
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+
+//         helper(root, result);
+
+//         return result;
+//     }
+
+//     private void helper(TreeNode root, List<Integer> result){
+//         if(root == null) return;
+
+//         helper(root.left, result);
+//         result.add(root.val);
+//         helper(root.right, result);
+//     }
+// }
