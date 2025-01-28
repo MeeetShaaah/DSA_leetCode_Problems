0616b4437cfd50ab75ba1b483b1b8c649b6a4c06
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2025-01-28 10:54:24 -0500

    Time: 1 ms (89.88%), Space: 45 MB (73.52%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,36 +15,57 @@
  */
 // We can solve this using 2 approaches, 1 - iterative(queue), 2 - recursive
 
-//  Approach - 1, Iterative using queue
+// Approach - 2, Recursive
 class Solution {
+    List<List<Integer>> levels = new ArrayList<>(); // class level initialization 
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return levels;
+        helper(root, 0);
+        return levels;
+    }
 
-        if(root == null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> list = new ArrayList<>();
-
-            for(int i = 1; i <= size; i++){
-                TreeNode node = queue.poll();
-
-                list.add(node.val);
-
-                if(node.left != null){
-                    queue.add(node.left);
-                }
-
-                if(node.right != null){
-                    queue.add(node.right);
-                }
-            }
-
-        result.add(list);
+    private void helper(TreeNode node, int level){
+        if(levels.size() == level){
+            levels.add(new ArrayList<Integer>());
         }
-        return result;
+
+        levels.get(level).add(node.val);
+
+        if(node.left != null) helper(node.left, level + 1);
+        if(node.right != null) helper(node.right, level + 1);
     }
 }
+
+//  Approach - 1, Iterative using queue
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> result = new ArrayList<>();
+
+//         if(root == null) return result;
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             List<Integer> list = new ArrayList<>();
+
+//             for(int i = 1; i <= size; i++){
+//                 TreeNode node = q.poll();
+
+//                 list.add(node.val);
+
+//                 if(node.left != null){
+//                     q.add(node.left);
+//                 }
+
+//                 if(node.right != null){
+//                     q.add(node.right);
+//                 }
+//             }
+//         result.add(list);
+//         }
+//     return result;
+//     }
+// }
