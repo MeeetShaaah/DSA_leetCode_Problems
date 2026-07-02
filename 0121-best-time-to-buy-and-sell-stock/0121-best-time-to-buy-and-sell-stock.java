782650be071bcf7362184cba59f27affc30956f0
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-01 23:40:12 -0700

    Time: 1 ms (99.94%), Space: 94.3 MB (70.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,36 +1,16 @@
-// Updated Version of my code.
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = prices[0];
-        int maxProfite = 0;
-        for (int sell = 1; sell < prices.length; sell++){
-            if (prices[sell] < buy){
-                buy = prices[sell];
-            }else if (prices[sell] - buy > maxProfite){
-                maxProfite = Math.max(maxProfite, prices[sell] - buy);
+        int buy = 0;
+        int max = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            
+            max = Math.max(max, prices[i] - prices[buy]);
+
+            if(prices[i] < prices[buy]){
+                buy = i;    
             }
         }
-        return maxProfite;
+        return max;
     }
 }
-
-
-//  this is my approch, 
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int buy = 0;
-//         int max = 0;
-        
-//         for (int sell = 1; sell < prices.length; sell++){
-//             if(prices[sell] < prices[buy]){
-//                 buy = sell ;
-//             }else {
-//                 int difference = prices[sell] - prices[buy];
-//                 max = Math.max(max, difference);
-//             }
-//         }
-//         return max;
-//     }
-// }
-
-// Approch - 1, Brute Force using 2 loops. 
