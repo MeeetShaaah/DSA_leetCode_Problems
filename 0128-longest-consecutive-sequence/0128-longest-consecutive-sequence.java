dec865cadbfd7f3e99fc3e28971f981d8590c8ed
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-03 13:29:19 -0700

    Time: 31 ms (61.61%), Space: 95.7 MB (27.9%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,50 +1,27 @@
-//Approch - 2, Using set, which take off the duplicates and then we 
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
+        HashSet<Integer> set = new HashSet<>();
 
-        Set<Integer> set = new HashSet<>();
-        int longest = 1;
-        for (int num : nums){
+        for(int num : nums){
             set.add(num);
         }
 
-        for (int element : set){
-            if (!set.contains(element - 1)){
-                int count = 1;
+        int start = -1;
+        int max = 0;
 
-                int x = element;
-                while (set.contains(x + 1)){
-                    count ++;
-                    x = x+1;
+        for(int element : set){
+            if(!set.contains(element-1)){
+                start = element;
+
+                int count = 1;
+                
+                while(set.contains(start+1)){
+                start ++;
+                count++;
                 }
-                longest = Math.max(longest, count);
+            max = Math.max(max,count);
             }
         }
-        return longest;
+    return max;
     }
 }
-// Approch - 1, sort the array and apply the logic, TC - O(nlogn) so i have to improve this to O(n)...!! 
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-//         Arrays.sort(nums);
-
-//         if (nums.length == 0) return 0;
-
-//         int count = 1; 
-//         int max = 1;
-
-//         for (int i = 1; i < nums.length; i++){
-//             if (nums[i] == nums[i-1]) continue;
-
-//             if(nums[i] - nums[i-1] == 1){
-//                 count ++;
-//             } else {
-//                 max = Math.max(max, count);
-//                 count = 1;
-//             }
-//         }
-        
-//         return Math.max(count, max);
-//     }
-// }
