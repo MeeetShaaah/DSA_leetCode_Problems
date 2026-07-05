ed90f3c5f722e100507345c0a9cd623937a6c05d
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-05 14:11:25 -0700

    Time: 25 ms (45.96%), Space: 48.5 MB (78.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,43 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        int sum = 0;
+        int prefixSum = 0;
         int count = 0;
 
         map.put(0,1);
-        for(int i =0; i < nums.length; i ++){
-            sum +=nums[i];
 
-            if (map.containsKey(sum-k)){
-                count += map.get(sum-k);
+        for(int i = 0; i < nums.length; i++){
+            prefixSum += nums[i];
+
+            if(map.containsKey(prefixSum - k)){
+                count += map.get(prefixSum - k);
             }
 
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
     }
 }
+
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         Map<Integer,Integer> map = new HashMap<>();// (prefixSum, frequency) mapping
+
+//         int prefixSum = 0;
+//         int count = 0;
+
+//         map.put(0,1); //this is for the condition when the prefix sum become 0, because we are checking sum - k, which eventually gives 0 sometimes so in that case we have to add additional condition. 
+
+//         for(int i = 0; i < nums.length; i++){
+//             prefixSum += nums[i];
+
+//             if(map.containsKey(prefixSum - k)){
+//                 count += map.get(prefixSum - k);
+//             }
+
+//             map.put(prefixSum, map.getOrDefault(prefixSum, 0 ) + 1);
+//         }
+//         return count;
+//     }
+// }
