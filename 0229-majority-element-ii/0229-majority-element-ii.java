65ca5d535ad3ed839bbd2dd36db520a1ad9c6442
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-05 16:10:09 -0700

    Time: 14 ms (41.45%), Space: 50.9 MB (98.59%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,59 +1,18 @@
-// Approch - 3, Extended Moor's voting Algorithm
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0;
-        int count2 = 0;
-        int el1 = Integer.MIN_VALUE;
-        int el2 = Integer.MIN_VALUE;
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
 
         for (int num : nums){
-            if (count1 == 0 && el2 != num){
-                count1 = 1;
-                el1 = num;
-            }else if (count2 == 0 && el1 != num){
-                count2 = 1;
-                el2 = num;
-            }else if (el1 == num) count1++;
-            else if (el2 == num) count2++;
-            else {count1--; count2--;}
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        count1 = 0;
-        count2 = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for (int num : nums){
-            if (el1 == num) count1++;
-            if (el2 == num) count2++;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if((entry.getValue()) > (n/3)){
+                list.add(entry.getKey());
+            }
         }
-        int mini = (int)(nums.length)/3 +1;
-        if (count1 >= mini) list.add(el1);
-        if (count2 >= mini) list.add(el2);
-
         return list;
     }
 }
-
-
-// Approch - 2, use hashmap, but this approch is not optimize to O(1) TC or SC...!!  
-// class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         ArrayList<Integer> list = new ArrayList<>();
-
-//         int mini = (int)(nums.length) / 3 + 1;
-
-//         for (int i = 0; i < nums.length; i++){
-//             map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
-            
-//             if(map.get(nums[i]) == mini){
-//                 list.add(nums[i]);
-//             }
-
-//             if (list.size() == 2) break;
-//         }
-//         return list;
-//     }
-// }
-
-// Approch - 1, Brute Force  use 1 loop to traverse, 2nd loop to check the element and update the frequency, and last check the count (even if (list.size() == 2) break; because think....!!!!)
