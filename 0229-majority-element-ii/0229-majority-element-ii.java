1676ac5e8f6ef2aa959f0f61c0af492ed2f78230
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-05 17:44:10 -0700

    Time: 2 ms (99.94%), Space: 52.2 MB (80.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-
-        int min = n/3 + 1;
-        for (int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            if(map.get(num) == min){
-                list.add(num);
+        int element1 = -1;
+        int element2 = -1;
+        int cnt1 = 0;
+        int cnt2 = 0;
+                                                                                
+        for(int num : nums){
+            if(num == element1){
+                cnt1++;
+            }else if(num == element2){
+                cnt2++;
+            }else if(cnt1 == 0){
+                element1 = num;
+                cnt1++;
+            }else if(cnt2 == 0){
+                element2 = num;
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
             }
         }
+
+        List<Integer> list = new ArrayList<>();
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for(int num : nums){
+            if(num == element1){
+                cnt1++;
+            }else if(num == element2){
+                cnt2++;
+            }
+        }
+
+        int min = nums.length/3;
+        if(cnt1 > min){
+            list.add(element1);
+        }
+        
+        if(cnt2 > min){
+            list.add(element2);
+        }
+
         return list;
     }
 }
