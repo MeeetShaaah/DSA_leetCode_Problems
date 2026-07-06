564fdb8956fc524116615f56b82cec5fb653c47e
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-06 13:15:57 -0700

    Time: 37 ms (24.63%), Space: 59.7 MB (21.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
+        Arrays.sort(nums);
+        // [-4,-1,-1,0,1,2]
         Set<List<Integer>> set = new HashSet<>();
 
-        Arrays.sort(nums); // [-4,-1,-1,0,1,2]
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
 
-        int n = nums.length;
+            int sum = 0;
+            int target = -nums[i];
 
-        for (int i = 0; i < n ; i++) {
-            Set<Integer> hashSet = new HashSet<>();
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            for(int j = i+1; j < n; j++){
-                int third = -(nums[i] + nums[j]);
+            while (left < right) {
 
-                if(hashSet.contains(third)){
-                    List<Integer> list = Arrays.asList(nums[i], nums[j], third);
-                    Collections.sort(list);
-                    set.add(list);
+                sum = nums[left] + nums[right];
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
                 }
-                hashSet.add(nums[j]);
             }
         }
-
         return new ArrayList<>(set);
     }
 }
