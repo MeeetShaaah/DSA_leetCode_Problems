b0367f7d35428c1898e2fe2d562e34505d636391
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-06 18:01:47 -0700

    Time: 12 ms (9.71%), Space: 49.1 MB (59.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,43 +1,25 @@
-// Approch - 2, based on the brute force approch itself, there is no way that without looking at the solution you get the idea that how to write this code, 
-
-//Step - 1, you habe to sort the whole multidimantial array, this is the unique method,   
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
 
-        // sort the given intervals.....using Arrays.sort() itself
-
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a, int[]  b){
-                return a[0] - b[0];
-            }
-        });
-
-        // now We gonna pick one element using for loop and we will compare that with previous added element in arraylist, and at the same time we gonnna add that element into to list with the conditions.
         List<int[]> list = new ArrayList<>();
-        
-        for (int i = 0; i < intervals.length; i++) {
-            // If the list is empty or the current interval does not overlap
-            if (list.isEmpty() || intervals[i][0] > list.get(list.size() - 1)[1]) {
-                list.add(new int[]{intervals[i][0], intervals[i][1]});
-            } else {
-                // Merge intervals by updating the end time of the last interval
-                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], intervals[i][1]);
+
+        int[] current = intervals[0];
+
+        for(int i = 1; i < intervals.length; i++){
+            
+            int[] next = intervals[i];
+
+            if(next[0] <= current[1]){
+                current[1] = Math.max(current[1], next[1]);
+            }else{
+                list.add(current);
+                current = next;
             }
         }
 
-        // Step 4: Convert the list to a 2D array and return
+        list.add(current);
+
         return list.toArray(new int[list.size()][]);
     }
 }
-
-//         for (int i = 0; i < intervals.length; i++){
-//             if(list.isEmpty() || intervals[i][0] > list.get(list.size() - 1).get(1)){
-//                 list.add(Arrays.asList(intervals[i][0], intervals[i][1]));
-//             }else {
-//                 list.get(list.size()-1).set(1, Math.max(list.get(list.size()-1).get(1), intervals[i][1]));
-//             }
-//         }
-//         return list.toArray(new int[list.size()][]);
-//     }
-// }
-
