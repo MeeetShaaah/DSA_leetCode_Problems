661a4e198907705dab67ce1147b86eaf488ce443
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-12 16:39:03 -0700

    Time: 14 ms (26.27%), Space: 46.7 MB (16.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,23 @@
+// for followup question 
 class Solution {
     public boolean isAnagram(String s, String t) {
+
         if(s.length() != t.length()) return false;
 
-        int[] counter = new int[26];
-        for(int i = 0; i < s.length(); i++){
-            counter[s.charAt(i) - 'a']++;
-            counter[t.charAt(i) - 'a']--;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
         }
 
-        for (int count : counter){
-            if (count !=0) return false;
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)-1);
+        }
+
+        for(int count : map.values()){
+            if(count != 0){
+                return false;
+            }
         }
         return true;
     }
