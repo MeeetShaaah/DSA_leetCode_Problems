01aa2ce92c8a042e22d0d2094af12e7a5639ac61
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-07-28 15:07:55 -0700

    Time: 1 ms (99.86%), Space: 65.1 MB (75.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,16 +1,29 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
+        int n = nums.length;
 
-        for(int i = 0, temp = 1; i< nums.length; i++){
-            answer[i] = temp;
-            temp *=nums[i];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        int[] result = new int[n];
+
+        int product = 1;
+        for(int i = 0; i < n; i++){
+            prefix[i] = product;
+
+            product *= nums[i];
         }
 
-        for(int i=nums.length-1, temp=1; i>=0; i--){
-            answer[i]*= temp;
-            temp *=nums[i];
+        product = 1;
+        for(int i = n - 1; i >= 0; i--){
+            suffix[i] = product;
+            product *= nums[i];
         }
-        return answer;
+
+        for(int i = 0; i < n; i++){
+            result[i] = prefix[i]*suffix[i];
+        }
+
+        return result;
     }
 }
