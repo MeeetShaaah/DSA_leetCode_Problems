c83792b991c8664cb7576b0aaf1c52d3b87857e5
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-08-09 15:22:43 -0700

    Time: 0 ms (100%), Space: 43.6 MB (92.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,3 +1,5 @@
+// in distinct value array, either of one half is always sorted
+
 class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
@@ -6,12 +8,12 @@ class Solution {
         while(left <= right){
             int mid = left + (right - left) / 2;
 
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
             }
 
             if(nums[left] <= nums[mid]){
-                if(target >= nums[left] && target < nums[mid] ){
+                if(target >= nums[left] && target < nums[mid]){
                     right = mid - 1;
                 }else{
                     left = mid + 1;
@@ -20,7 +22,7 @@ class Solution {
                 if(target > nums[mid] && target <= nums[right]){
                     left = mid + 1;
                 }else{
-                    right = mid - 1;
+                    right  = mid - 1;
                 }
             }
         }
