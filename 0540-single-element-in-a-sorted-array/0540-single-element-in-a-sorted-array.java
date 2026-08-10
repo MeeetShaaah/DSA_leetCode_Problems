1c3f01841959a66b6a74a960aceb3ef1e4d26e7b
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-08-10 14:26:55 -0700

    Time: 0 ms (100%), Space: 52.7 MB (78.06%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,36 +1,21 @@
-//Approach - 2, You forgot that you can also use xOR (Shame...)
-//Approach - 1, linear search, and compare the elements,
-//Approach - 3, binary serach, go through this solution (not getting this ** part)
-
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length; 
-
-        if(n == 1){
-            return nums[0];
-        }else if(nums[0]!=nums[1]){
-            return nums[0];
-        }else if(nums[n-1] != nums[n-2]){
-            return nums[n-1];
-        }
-
         int left = 0;
-        int right = n - 1;
+        int right = nums.length - 1;
 
-        while (left <= right){
-            int mid = left + (right - left)/2;
+        while(left < right){
+            int mid = left + (right - left) / 2;
 
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
-                return nums[mid];
+            if(mid % 2 == 1){
+                mid--;
             }
 
-            // ** i am not geting this conditions...
-            if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] == nums[mid+1])){
-                left = mid + 1;
-            }else {
-                right = mid - 1;
+            if(nums[mid] == nums[mid+1]){
+                left = mid + 2;
+            }else{
+                right = mid;
             }
         }
-        return -1;
+        return nums[left];
     }
 }
