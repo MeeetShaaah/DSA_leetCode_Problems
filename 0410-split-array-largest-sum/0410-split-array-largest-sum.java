4245865223e1ab589c3ad8b6493299ddf1b2e5b0
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-08-16 16:33:25 -0700

    Time: 0 ms (100%), Space: 43.1 MB (51.56%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,76 +1,40 @@
-// Go thriugh this again please...
 class Solution {
-    public static int countPartitions(int[] a, int maxSum) {
-        int n = a.length; //size of array.
-        int partitions = 1;
-        long subarraySum = 0;
-        for (int i = 0; i < n; i++) {
-            if (subarraySum + a[i] <= maxSum) {
-                //insert element to current subarray
-                subarraySum += a[i];
-            } else {
-                //insert element to next subarray
-                partitions++;
-                subarraySum = a[i];
-            }
-        }
-        return partitions;
-    }
-    
-    public int splitArray(int[] a, int k) {
-        int low = a[0];
-        int high = 0;
-        //find maximum and summation:
-        for (int i = 0; i < a.length; i++) {
-            low = Math.max(low, a[i]);
-            high += a[i];
+    public int splitArray(int[] nums, int k) {
+        int max = 0;
+        int sum = 0;
+
+        for(int num : nums){
+            max = Math.max(num, max);
+            sum += num;
         }
 
-        //Apply binary search:
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int partitions = countPartitions(a, mid);
-            if (partitions > k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        long left =  max;
+        long right = sum;
+
+        while (left <= right){
+            long mid = left + (right - left) / 2;
+
+            if(countSubarraySum(nums, mid) <= k){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
             }
         }
-        return low;
+        return (int)left;
+    }
+
+    public long countSubarraySum(int[] nums, long maxSum){
+        long count = 1;
+        long sum = 0;
+
+        for(int num : nums){
+            if(sum + num > maxSum){
+                count++;
+                sum = num;
+            }else{
+                sum += num;
+            }
+        }
+        return count;
     }
 }
-
-// Brute Force, 
-// class Solution {
-//     public static int countPartitions(int[] a, int maxSum) {
-//         int n = a.length; //size of array.
-//         int partitions = 1;
-//         long subarraySum = 0;
-//         for (int i = 0; i < n; i++) {
-//             if (subarraySum + a[i] <= maxSum) {
-//                 //insert element to current subarray
-//                 subarraySum += a[i];
-//             } else {
-//                 //insert element to next subarray
-//                 partitions++;
-//                 subarraySum = a[i];
-//             }
-//         }
-//         return partitions;
-//     }
-//     public int splitArray(int[] a, int k) {
-//         int low = a[0];
-//         int high = 0;
-//         //find maximum and summation:
-//         for (int i = 0; i < a.length; i++) {
-//             low = Math.max(low, a[i]);
-//             high += a[i];
-//         }
-
-//         for (int maxSum = low; maxSum <= high; maxSum++) {
-//             if (countPartitions(a, maxSum) == k)
-//                 return maxSum;
-//         }
-//         return low;
-//     }
-// }
