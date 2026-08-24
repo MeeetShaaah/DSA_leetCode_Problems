91412345de369a7cfcb759174c5241c8be3050f6
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-08-24 13:36:55 -0700

    Time: 0 ms (100%), Space: 42.6 MB (85.79%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses-08-24-2026-13-36-56.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDepth(String s) {
+        int max = 0;
+        int counter = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                counter++;
+            }
+
+            if (ch == ')') {
+                counter--;
+            }
+
+            max = Math.max(max, counter);
+        }
+        return max;
+    }
+}
