b5c2b53beba32eba160f20787cdc0492c3e8ec79
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-08-31 14:37:59 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in Linked List/search-in-linked-list.java
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.java
@@ -1,9 +1,4 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
+/* Structure of Linked List Node
 class Node {
     int data;
     Node next;
@@ -12,55 +7,19 @@ class Node {
         data = x;
         next = null;
     }
+}*/
 
-} class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            int n = sc.nextInt();
-            Node head = new Node(sc.nextInt());
-            Node tail = head;
-            for (int i = 0; i < n - 1; i++) {
-                tail.next = new Node(sc.nextInt());
-                tail = tail.next;
-            }
-            int key = sc.nextInt();
-            Solution g = new Solution();
-            boolean ans = g.searchKey(n, head, key);
-            System.out.println(ans ? "1" : "0");
-            // printList(head);
-            t--;
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
-/* Node of a linked list
-  class Node {
-   int data;
-    Node next;
-    Node(int d)  { data = d;  next = null; }
-}
-*/
 class Solution {
-    static boolean searchKey(int n, Node head, int key) {
+    public boolean searchKey(Node head, int key) {
         // Code here
+        Node temp = head;
         
-        Node current = head;
-
-        for (int i = 0; i < n; i++){
-            if (current.data == key){ 
+        while(temp != null){
+            if(temp.data == key){
                 return true;
-                
             }
             
-            current = current.next;
+            temp = temp.next;
         }
         return false;
     }
