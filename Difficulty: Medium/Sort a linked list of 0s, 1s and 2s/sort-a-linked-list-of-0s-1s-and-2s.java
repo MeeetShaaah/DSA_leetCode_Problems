35d7041e7920ff7a45aeacf8da01c2cb3269ba9a
Meet Shaah <49854784+MeeetShaaah@users.noreply.github.com>
2026-09-16 15:20:06 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -0,0 +1,59 @@
+/*
+class Node {
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+} */
+
+class Solution {
+	public Node segregate(Node head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		
+		Node zeroDummy = new Node(-1);
+		Node oneDummy = new Node(-1);
+		Node twoDummy = new Node(-1);
+		
+		Node zeroTail = zeroDummy;
+		Node oneTail = oneDummy;
+		Node twoTail = twoDummy;
+		
+		Node reader = head;
+		
+		while (reader != null) {
+			
+			Node next = reader.next;
+			reader.next = null;
+			
+			if (reader.data == 0) {
+				zeroTail.next = reader;
+				zeroTail = zeroTail.next;
+			}
+			else if (reader.data == 1) {
+				oneTail.next = reader;
+				oneTail = oneTail.next;
+			}
+			else {
+				twoTail.next = reader;
+				twoTail = twoTail.next;
+			}
+			
+			reader = next;
+		}
+		
+		// 0 → 1 → 2
+		zeroTail.next = (oneDummy.next != null)
+		? oneDummy.next
+		: twoDummy.next;
+		
+		oneTail.next = twoDummy.next;
+		
+		return zeroDummy.next;
+	}
+}
